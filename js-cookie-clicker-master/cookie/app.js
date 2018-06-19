ed2bae01e775e11e350be439c110f58fa380d6da
SIMOCCAuch19 <clerc6nathan@hotmail.fr>
2018-06-19 14:50:00 +0200

    Part 11 fait

---
+++ js-cookie-clicker-master/cookie/app.js
@@ -0,0 +1,43 @@
+var clic =$('#clickme');
+var autoclic=$('#autoclick');
+var div = $('#display');
+var multiply= $('#multiply');
+var compteur = 1;
+var score = 0;
+var mult= 50;
+var auto = 0;
+var prixauto=200;
+
+clic.click( function(){
+	score = score+1 *compteur;
+	div.html(score);
+
+});
+
+multiply.click( function incrementer(){
+	
+	if(score>=mult){
+		compteur ++;
+		score = score - mult;
+		mult = mult * 2;
+		div.html(score);
+		multiply.html('Multiplicateur x' + compteur + ' Prix ' + mult);
+	} else{
+		alert('Votre score est insuffisant!');
+	}
+});
+
+autoclic.click(function(){
+	if (score>=500) {
+		auto++;
+		score = score - prixauto;
+		setInterval(function(){
+			 score = score +auto;
+			 div.html(score);
+		}, 1000);
+	}
+	else {
+			alert('Votre score est insuffisant');
+		}
+	div.html(score);
+});
